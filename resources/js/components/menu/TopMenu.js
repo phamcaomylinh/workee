02e8a289636bd5bf650f6ea86d4b94f10ffc43f2
phamcaomylinh <phamcaomylinh@gmail.com>
2020-09-08 07:55:59 +0700

    fix some style in layout

--- resources/js/components/menu/TopMenu.js
+++ resources/js/components/menu/TopMenu.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { Menu } from 'antd';
-import Logo from '../logo/logo';
-import SubMenu from 'antd/lib/menu/SubMenu';
-import { UserOutlined } from '@ant-design/icons';
+import React from "react";
+import { Menu } from "antd";
+import Logo from "../logo/logo";
+import SubMenu from "antd/lib/menu/SubMenu";
+import { UserOutlined } from "@ant-design/icons";
+import { Link, NavLink } from "react-router-dom";
 export default function TopMenu() {
-    return(
+    return (
         <Menu className="top-menu" mode="horizontal">
             <Menu.Item>
-                <Logo />
+                <NavLink to="/">
+                    <Logo />
+                </NavLink>
             </Menu.Item>
             <Menu.Item>
-                Item 1
+                <NavLink to="/">Home</NavLink>
             </Menu.Item>
             <Menu.Item>
-                Item 3
+                <NavLink to="/about">About</NavLink>
             </Menu.Item>
-            <SubMenu icon={<UserOutlined />} title="Profile">
+
+            <SubMenu
+                style={{ float: "right" }}
+                icon={<UserOutlined />}
+                title="Profile"
+            >
                 <Menu.Item>My Profile</Menu.Item>
                 <Menu.Item>Log Out</Menu.Item>
             </SubMenu>
         </Menu>
-    )
+    );
 }
